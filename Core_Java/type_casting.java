7907a2b492dd8a0f11de364e6c3e0b955c73aa63
Imperial X <merajunnesha123@gmail..com>
2025-05-20 00:47:51 +0530

    type casting

---
+++ Core_Java/type_casting.java
@@ -0,0 +1,22 @@
+class cast{
+
+    public static void main(String arr[]){
+        byte b = 127;
+        int a = 258;
+        // explicit type casting
+        b = (byte)a; // result in the num%256
+        // implicit type casting
+        // a = b;
+        float f = 5.6f;
+        int num = (int)f; // losing the valus after the point
+
+        byte num1 = 30;
+        byte num2 = 10;
+
+        int result = num1*num2; // type promotion
+        System.out.println(result);
+
+        System.out.println(b);
+        System.out.println(num);
+    }
+}
