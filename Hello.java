860d04769d5a0548d5317f733d4a66ea2c43f6c5
Imperial X <merajunnesha123@gmail..com>
2025-05-20 00:06:42 +0530

    Initial Commit

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/* 
+PS D:\Java> jshell
+|  Welcome to JShell -- Version 21.0.7
+|  For an introduction type: /help intro
+
+jshell> 10+3
+$1 ==> 13
+
+jshell> 10-3
+$2 ==> 7
+
+jshell> System.out.print(10)
+10
+jshell> System.out.print("This is jshell");
+This is jshell
+*/
+
+class Hello{
+    public static void main(String a[]){
+        System.out.print("Hello World");
+    }
+
+}
