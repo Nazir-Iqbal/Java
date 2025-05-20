4780559304839e2e3298be1010ea93912d00072f
Imperial X <merajunnesha123@gmail..com>
2025-05-21 02:26:35 +0530

    loop

---
+++ Core_Java/loop.java
@@ -0,0 +1,35 @@
+class demo
+{
+    public static void main(String[] args)
+    {
+        // int i = 1;
+        
+        // nested while loop
+        // while(i<=4){
+        //     System.out.println("Hi " + i);
+            
+        //     int j = 1;
+        //     while(j<=3){
+        //         System.out.println("Hello " + j);
+        //         j++;
+        //     }
+
+        //     i++;
+        // }
+
+        // System.out.println("Bye " + i);
+
+        // int i = 5;
+
+        // do{
+        //     System.out.println("Hi "+i);
+        //     i++;
+        // }while(i<=4);
+
+        // for loop
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            System.out.println("Day: " + i);
+        }
+    }
+}
